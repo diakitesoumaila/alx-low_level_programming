472547a2a031133ea72781c4d36cd3021ab25a8b
diakitesoumaila <diakitesarah500@gmail.com>
2022-10-15 12:51:05 -0700

    Positive anything

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ int main(void)
 
 	if (n > 0)
 	{
-		ptintf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
@@ -28,4 +28,3 @@ int main(void)
 
 	return (0);
 }
-
